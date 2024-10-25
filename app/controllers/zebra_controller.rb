dd6693f9162a211599421c16f3c6a0f6de26e8e3
JuliaCraig03 <juliacraig@uchicago.edu>
2024-10-25 18:05:17 +0000

    Final

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,82 +1,54 @@
 class ZebraController < ApplicationController
   def homepage
-    get("/dice/:number_of_dice/:how_many_sides") do
-      @num_dice = params.fetch("number_of_dice").to_i
-     
-      @sides = params.fetch("how_many_sides").to_i
-      
-      @rolls = []
-    
-      @num_dice.times do
-        die = rand(1..@sides)
-    
-        @rolls.push(die)
-      end
     render({ :template => "game_templates/homepage" })
-  end 
+   end
 
-  def six
-    get("/dice/:number_of_dice/:how_many_sides") do
-      @num_dice = params.fetch("number_of_dice").to_i
-     
-      @sides = params.fetch("how_many_sides").to_i
-      
-      @rolls = []
+  def water
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+    @rolls = []
     
-      @num_dice.times do
-        die = rand(1..@sides)
-    
-        @rolls.push(die)
-      end
-    render({ :template => "game_templates/2_6" })
+    @num_dice.times do
+      die = rand(1..@sides)
+      @rolls.push(die)
+    end
+    render({ :template => "game_templates/flexible" })
   end
   
   def ten
-    get("/dice/:number_of_dice/:how_many_sides") do
-      @num_dice = params.fetch("number_of_dice").to_i
-     
-      @sides = params.fetch("how_many_sides").to_i
-      
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
       @rolls = []
-    
+
       @num_dice.times do
         die = rand(1..@sides)
-    
         @rolls.push(die)
       end
     render({ :template => "game_templates/2_10" })
   end
   
   def twenty
-    get("/dice/:number_of_dice/:how_many_sides") do
-      @num_dice = params.fetch("number_of_dice").to_i
-     
-      @sides = params.fetch("how_many_sides").to_i
-      
+    @num_dice = params.fetch("number_of_dice").to_i
+      @sides = params.fetch("how_many_sides").to_i 
       @rolls = []
-    
+
       @num_dice.times do
         die = rand(1..@sides)
-    
         @rolls.push(die)
       end
     render({ :template => "game_templates/1_20" })
   end 
 
+
   def four
-    get("/dice/:number_of_dice/:how_many_sides") do
-      @num_dice = params.fetch("number_of_dice").to_i
-     
-      @sides = params.fetch("how_many_sides").to_i
-      
-      @rolls = []
-    
-      @num_dice.times do
-        die = rand(1..@sides)
-    
-        @rolls.push(die)
-      end
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides)
+      @rolls.push(die)
+    end
     render({ :template => "game_templates/5_4" })
   end 
-
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "zebra", :action => "homepage"})
 
-  get("/dice/2/6", { :controller => "zebra", :action => "six"})
+  get("/dice/:number_of_dice/:how_many_sides", { :controller => "zebra", :action => "water"})
   
-
   get("/dice/2/10", { :controller => "zebra", :action => "ten"})
-
 
   get("/dice/1/20", { :controller => "zebra", :action => "twenty"}) 
   
-
   get("/dice/5/4", { :controller => "zebra", :action => "four"})
 end 
